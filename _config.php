9fb8131699ce9d9e4c926d0b3f7ccf4707d90597
Senorgeno <genedower@gmail.com>
2018-10-03 12:38:58 +1300

    earlier SS fallback

--- _config.php
+++ _config.php
@@ -1,5 +1,9 @@
 <?php
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 $dir = basename(dirname(__FILE__));
 if($dir != "dashboard") {
 	user_error('Dashboard: Directory name must be "dashboard" (currently "'.$dir.'")',E_USER_ERROR);
